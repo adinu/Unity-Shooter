f48a28b3377e4f00970dbe69e6c5ac368b8ad8e0
SergeOxe <sergeoxe@gmail.com>
2014-12-27 13:59:07 +0200

    Enemies with parachute

--- Project/Assets/Scripts/Enemies/EnemyMovment.cs
+++ Project/Assets/Scripts/Enemies/EnemyMovment.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class EnemyMovment : MonoBehaviour {
 	//Vector3 movment = new Vector3(Random.Range(-3f,3f),Random.Range(-1f,0f),0);
-	Vector3 movment = new Vector3(0,Random.Range(-1f,0f),0);
+	Vector3 movment = new Vector3(0,Random.Range(-0.4f,0f),0);
 	public float speed;
 	void FixedUpdate () {
 		this.rigidbody2D.velocity = movment * speed;
