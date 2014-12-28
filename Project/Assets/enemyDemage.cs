6f6fa73a9704f4b8344e3a5bfbc314c857d72f7d
SergeOxe <sergeoxe@gmail.com>
2014-12-28 12:55:19 +0200

    Fix bug

---
+++ Project/Assets/enemyDemage.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyDemage : MonoBehaviour {
+	public int Lifes = 2;
+
+	private GameObject GameController;
+	public GameObject points;
+	
+	
+	void Start(){
+		GameController = GameObject.FindGameObjectWithTag ("GameController");
+	}
+	
+
+
+	// Use this for initialization
+	public void Hit () {
+		Lifes--;
+		if (Lifes == 0)
+			Kill ();	
+	}
+	
+	// Update is called once per frame
+	private void Kill () {
+		Destroy (this);
+		Instantiate(points,this.transform.position,Quaternion.identity);
+		GameController.GetComponent<Controller>().addScore(10);
+	}
+}
